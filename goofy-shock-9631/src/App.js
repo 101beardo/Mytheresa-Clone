ae0253233fde9deff217df97ea75fd92bf104a8f
Tarun Sharma <101tash101@gmail.com>
2022-12-15 00:00:28 +0530

    Revert "n"

--- goofy-shock-9631/src/App.js
+++ goofy-shock-9631/src/App.js
@@ -1,11 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-import WomenPage from './Components/WomenPage';
 
 function App() {
   return (
     <div className="App">
-     <WomenPage/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
