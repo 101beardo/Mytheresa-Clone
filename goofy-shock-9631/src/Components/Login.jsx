94562fa934f2cb367241cb468e251fe68b540020
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2022-12-17 02:20:44 +0530

    day-4 Shubham

--- goofy-shock-9631/src/Components/Login.jsx
+++ goofy-shock-9631/src/Components/Login.jsx
@@ -1,12 +1,120 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "../Redux/context/AuthContext";
+import axios from '../Api/axios';
+import "../Styles/Login.css"
+import { Link } from 'react-router-dom';
+
+const LOGIN_URL = '/auth';
 
 const Login = () => {
-  return (
-    <Box>
-     
-    </Box>
-  )
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response));
+            const accessToken = response?.data?.accessToken;
+            const roles = response?.data?.roles;
+            setAuth({ user, pwd, roles, accessToken });
+            setUser('');
+            setPwd('');
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
+    }
+
+    return (
+        <div className='toplogindiv'>
+            <div>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1 style={{fontSize:"19px",fontWeight:"600"}}>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                        />
+
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                        />
+                        <Link to="/">
+
+                        <button className='signin'>Sign In</button>
+                        </Link>
+                    </form>
+                    <p>
+                        Need an Account?<br />
+                        <span className="line">
+                           
+                            <Link to={"/register"}>
+                            <button className='signin'>Sign Up</button>
+                            </Link>
+                        </span>
+                    </p>
+                    <div className='google'>
+                    <h1>or connect via</h1>
+                    <img style={{width:"50px",marginTop:"20px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1200px-Google_%22G%22_Logo.svg.png" alt="google" />
+                    </div>
+                </section>
+            )}
+            </div>
+        </div>
+    )
 }
 
 export default Login
