e5c567dadcbe43061d8a7081957d534f62eb4def
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2022-12-18 01:40:58 +0530

    day4 Shubham

--- goofy-shock-9631/src/App.js
+++ goofy-shock-9631/src/App.js
@@ -1,10 +1,6 @@
 
 import './App.css';
-import Login from './Components/Login';
-import LoginRegister from './Components/Login & Register';
-import InitialFocus from './Components/LoginModal';
 import PhoneIcon from './Components/PhoneIcon';
-import Register from './Components/Register';
 import MainRoutes from './Pages/MainRoutes';
 
 
@@ -13,12 +9,8 @@ function App(){
   return (
 
     <div className="App">
-      {/* <PhoneIcon/>
-      <MainRoutes/> */}
-      {/* <Login /> */}
-      {/* <Register /> */}
-      {/* <LoginRegister/> */}
-      <InitialFocus />
+      <PhoneIcon/>
+      <MainRoutes/>
     </div>
 
   );
