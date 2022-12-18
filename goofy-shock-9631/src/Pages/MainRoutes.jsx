cca009bfe7067c488468c5a50c9b58ec9fec4582
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2022-12-18 17:14:24 +0530

    n

--- goofy-shock-9631/src/Pages/MainRoutes.jsx
+++ goofy-shock-9631/src/Pages/MainRoutes.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Dropdown from '../Components/Dropdown'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
+import Admin from './Admin'
 import { Kids } from './Kids'
 import Life from './Life'
 import Mens from './Mens'
 import Wishlist from './Wishlist'
-
 import WomenPage from './WomenPage'
-// import Sidebar from './Sidebar';
 const MainRoutes = () => {
   return (
     <div>
      <Navbar/>
-  
+     <Dropdown/>
       <Routes>
+        <Route path='/admin' element={<Admin/>}/>
         <Route path='/mens' element={<Mens/>}/>
         <Route path='/kids' element={<Kids/>}/>
         <Route path='/womens' element={<WomenPage/>}/>
         <Route path='/life' element={<Life />}/>
         <Route path='/wishlist' element={<Wishlist/>}/>
-        {/* <Route path='/sidebar' element={<Sidebar/>}/> */}
-     </Routes>
+      </Routes>
       <Footer/>
     </div>
   )
