8020509abaa01c3467a6d172c889167726c18131
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2022-12-21 22:39:57 +0530

    n

--- goofy-shock-9631/src/Components/Navbar.jsx
+++ goofy-shock-9631/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Link, useDisclosure,Button, Collapse, Icon, Image} from '@chakra-ui/react'
+import {Select,ModalOverlay,ModalFooter,ModalBody,Text,Box, Modal,ModalContent,ModalHeader,ModalCloseButton, Link, useDisclosure,Button, Collapse, Icon, Image} from '@chakra-ui/react'
 import {GiHamburgerMenu} from "react-icons/gi"
 import {CgClose} from "react-icons/cg"
 import { useNavigate } from 'react-router'
@@ -10,7 +10,7 @@ import { useNavigate } from 'react-router'
 const Navbar = () => {
      // border="1px solid red"
 
-  const { isOpen, onToggle } = useDisclosure()
+  const { isOpen, onToggle,onClose,onOpen } = useDisclosure()
   const navigate=useNavigate()
 
   return (
@@ -155,42 +155,7 @@ const Navbar = () => {
           </Modal>
           </Box> 
         </Box>
-        {/* <Box w="100%" 
-          h="70px" 
-          p="20px"
-          display={["flex","flex","none"]}
-          // position={"fixed"}
-          fontSize='20px'
-          zIndex={1}
-          align="right">
-                <Box w="100%">
-                        {isOpen ? <Button  onClick={onToggle}   p="10px" ><Icon boxSize="25px" as={CgClose}/></Button> : <Button  onClick={onToggle}   p="10px" ><Icon boxSize="25px" as={GiHamburgerMenu}/></Button> } 
-       
-                        <Collapse in={isOpen} animateOpacity>
-                                <Box
-                                    p='90px'
-                                    mt='4'
-                                    bg='white'
-                                    rounded='md'
-                                    shadow='dark-lg'
-                                    zIndex={2}
-                                    display="flex"
-                                    flexDirection="column"
-                                    align="center"
-                                    >
-              
-                                    <Link m="10px" style={{ textDecoration: 'none' }} onClick={onToggle}  href='#'>WOMEN</Link>
-            
-                                    <Link m="10px" style={{ textDecoration: 'none' }} onClick={onToggle}  href="#men">MEN</Link>
-              
-                                    <Link m="10px" style={{ textDecoration: 'none' }} onClick={onToggle}   href="#kids">KIDS</Link>
-              
-                                    <Link m="10px" style={{ textDecoration: 'none' }} onClick={onToggle}   href="#">LIFE</Link> 
-                                </Box>
-                            </Collapse>
-                    </Box>
-          
-        </Box> */}
+      
       <Image  mt="30px"  w="22%" src='https://www.mytheresa.com/skin/frontend/mytheresa/default/images/logo.png?v=20221130T110112'/>
       </Box>
 
