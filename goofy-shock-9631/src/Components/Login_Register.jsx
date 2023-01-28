ccb76e2983ffc6dfdc537d744b295819ab9e2870
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2023-01-28 13:15:01 +0530

    Login_&_Register Page are done with firebase

--- goofy-shock-9631/src/Components/Login_Register.jsx
+++ goofy-shock-9631/src/Components/Login_Register.jsx
@@ -1,25 +1,55 @@
-import React, { useState } from 'react'
+import React, {useRef, useState } from 'react'
 import { Box, Text, Input, Button, Checkbox,Select, Stack, Radio, RadioGroup} from '@chakra-ui/react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { login } from '../Redux/AuthReducer/action'
+import {useSelector, useDispatch } from 'react-redux'
+import { login } from "./firebase-config";
+import { signup } from "./firebase-config";
+import { userEmail} from '../Redux/AuthReducer/action'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const LoginRegister = () => {
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  const dispatch=useDispatch()
+
+  const auth  = useSelector((store)=> store.isAuth );
+  const dispatch = useDispatch();
+
+  const [loading,setloading]=useState(false);
+  const [userNotFound,setuserNotFount]=useState(false);
+  
+  const emailRef=useRef();
+  const passwordRef=useRef();
+  const usenavigate=useNavigate()
   const navigate=useNavigate()
 
+  async function signin(){
+    setloading(true)
+    dispatch( userEmail(emailRef.current.value));
+    try{
+        await login(emailRef.current.value,passwordRef.current.value)
+        alert("Login Succesful")
+        usenavigate("/adminpannel");
 
-
-  const handleSubmit=()=>{
- 
-    if(email&&password){
-      dispatch(login({email,password})).then(r=>{
-        navigate("/womens")
-      })
+    }catch(error){
+        setuserNotFount(true)
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        alert("User not Exist")
     }
+    setloading(false)
+}
+
+async function createuser(){
+  setloading(true)
+  try{
+      await signup(emailRef.current.value,passwordRef.current.value)
+      alert("user created")
+      navigate("/login")
+  }catch{        
+      alert("user already exist")
   }
+  setloading(false)
+}
+
+
+
   return (
     <Box id="Login_Register">    
     <Box
@@ -51,12 +81,12 @@ const LoginRegister = () => {
                 </Select>
                 <Input mt="20px" placeholder='first name *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
                 <Input mt="20px" placeholder='last name *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
-                <Input mt="20px" placeholder='Email address *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
-                <Input mt="10px" decoration="none" placeholder='Password *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
+                <Input mt="20px" ref={emailRef} placeholder='Email address *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
+                <Input mt="10px" ref={passwordRef}  decoration="none" placeholder='Password *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
                 <Text mt="15px" opacity="0.8"><Checkbox mt="3px"></Checkbox>  From now on I will receive the Mytheresa Newsletter with selected fashion offers. If I do not wish to receive the newsletter, I can unsubscribe at any time free of charge at privacy@mytheresa.com.</Text>
                 <Text mt="15px" opacity="0.8">I agree that Mytheresa may insert analytical web beacons into the newsletter and create a personalized user profile based on my purchase and usage behavior, including sending a notification when I have placed something in the shopping cart. Further details can be found in our Privacy Policy, clause 5. I understand that I can revoke my consent at any time by emailing privacy@mytheresa.com.</Text>
                 <Text mt="15px" opacity="0.8">* required fields</Text>
-                <Button mt="25px" padding="15px">REGISTER</Button>
+                <Button mt="25px" padding="15px" onClick={createuser} disabled={loading}>REGISTER</Button>
             </Box>
   
          {/* border */}
@@ -71,11 +101,11 @@ const LoginRegister = () => {
         <Box w={["null","null","50%"]} padding="5%" id="login">
             <Text fontSize="16.38px" opacity="0.8"> ALREADY REGISTERED?</Text>
             <Text mt="10px" fontSize="14px" opacity="0.8"> If you have an account with us, please log in. </Text>
-            <Input mt="20px" value={email} onChange={e=>setEmail(e.target.value)} placeholder='Email address *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
-            <Input mt="10px" type="password" value={password} onChange={e=>setPassword(e.target.value)} decoration="none" placeholder='Password *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
+            <Input mt="20px"  ref={emailRef} placeholder='Email address *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
+            <Input mt="10px" type="password" ref={passwordRef}   decoration="none" placeholder='Password *' color="rgb(117, 117, 117)" background-color="#fbfbfb"></Input>
             <Text mt="5px" opacity="0.8">* required fields</Text>
             <Text mt="10px" opacity="0.8" _hover={{cursor:"pointer", opacity:"1" }}>Forgot Your Password?</Text>
-            <Button color="white" bg="black" mt="20px" onClick={handleSubmit} >LOGIN</Button>
+            <Button color="white" bg="black" mt="20px" disabled={loading} onClick={signin}>LOGIN</Button>
             <Text mt="15px" opacity="0.8">At Mytheresa, we keep your information secure. As a result, it will be necessary for you to log in to your account before you are able to place an order or change the information on your account. You will be asked to log in to complete your order even if you have already been greeted on the website.</Text>
         </Box>
     </Box>
