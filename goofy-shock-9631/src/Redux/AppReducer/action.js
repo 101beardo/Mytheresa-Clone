c7722adc6d16bde9baba650d6db71012670abce3
Tarun Sharma <101tash101@gmail.com>
2023-02-15 20:29:41 +0530

    json-server updated

--- goofy-shock-9631/src/Redux/AppReducer/action.js
+++ goofy-shock-9631/src/Redux/AppReducer/action.js
@@ -19,7 +19,7 @@ const getDataError=()=>{
 
 const getdata=(params)=>(dispatch)=>{
     dispatch(getDataRequest());
-    return axios.get('https://jsondeployment-lyna.vercel.app/mens',params)
+    return axios.get('https://json-server-employee-crud-seven.vercel.app/mens',params)
     .then((res)=>dispatch(getDataSuccess(res.data)))
     .catch((error)=>{
         dispatch(getDataError(error))
@@ -52,7 +52,7 @@ const getWishDataError=()=>{
 
 const getWishData=()=>(dispatch)=>{
     dispatch(getWishDataRequest());
-    return axios.get('https://jsondeployment-lyna.vercel.app/wish')
+    return axios.get('https://json-server-employee-crud-seven.vercel.app/wish')
     .then((res)=>dispatch(getWishDataSuccess(res.data)))
     .catch((error)=>{
         dispatch(getWishDataError(error))
@@ -62,7 +62,7 @@ const getWishData=()=>(dispatch)=>{
 const addWishData=(payload)=>(dispatch)=>{
     dispatch({type:types.ADD_WISH_DATA_REQUEST})
     return axios
-    .post('https://jsondeployment-lyna.vercel.app/wish',payload)
+    .post('https://json-server-employee-crud-seven.vercel.app/wish',payload)
     .then((r)=>{
         dispatch({type:types.ADD_WISH_DATA_SUCCESS,payload:r.data})
     })
@@ -96,7 +96,7 @@ const getCartDataError=()=>{
 
 const getCartData=()=>(dispatch)=>{
     dispatch(getCartDataRequest());
-    return axios.get('https://jsondeployment-lyna.vercel.app/bag')
+    return axios.get('https://json-server-employee-crud-seven.vercel.app/bag')
     .then((res)=>dispatch(getCartDataSuccess(res.data)))
     .catch((error)=>{
         dispatch(getCartDataError(error))
@@ -106,7 +106,7 @@ const getCartData=()=>(dispatch)=>{
 const addCartData=(payload)=>(dispatch)=>{
     dispatch({type:types.ADD_CART_DATA_REQUEST})
     return axios
-    .post('https://jsondeployment-lyna.vercel.app/bag',payload)
+    .post('https://json-server-employee-crud-seven.vercel.app/bag',payload)
     .then((r)=>{
         dispatch({type:types.ADD_CART_DATA_SUCCESS,payload:r.data})
     })
@@ -120,7 +120,7 @@ const addCartData=(payload)=>(dispatch)=>{
 const addData=(payload)=>(dispatch)=>{
     dispatch({type:types.ADD_DATA_REQUEST})
     return axios
-    .post('https://jsondeployment-lyna.vercel.app/mens',payload)
+    .post('https://json-server-employee-crud-seven.vercel.app/mens',payload)
     .then((r)=>{
         dispatch({type:types.ADD_DATA_SUCCESS,payload:r.data})
     })
@@ -134,7 +134,7 @@ const addData=(payload)=>(dispatch)=>{
 const updateData=(id,payload)=>(dispatch)=>{
     dispatch({type:types.UPDATE_DATA_REQUEST})
     return axios
-    .patch(`https://jsondeployment-lyna.vercel.app/mens/${id}`,payload)
+    .patch(`https://json-server-employee-crud-seven.vercel.app/mens/${id}`,payload)
     .then((r)=>{
         dispatch({type:types.UPDATE_DATA_SUCCESS,payload:r.data})
     })
@@ -147,7 +147,7 @@ const updateData=(id,payload)=>(dispatch)=>{
 const deleteData=(id)=>(dispatch)=>{
     dispatch({type:types.DELETE_DATA_REQUEST})
     return axios
-    .delete(`https://jsondeployment-lyna.vercel.app/mens/${id}`)
+    .delete(`https://json-server-employee-crud-seven.vercel.app/mens/${id}`)
     .then((r)=>{
         dispatch({type:types.DELETE_DATA_SUCCESS,payload:id})
     })
@@ -160,7 +160,7 @@ const deleteData=(id)=>(dispatch)=>{
 const deleteCartData=(id)=>(dispatch)=>{
     dispatch({type:types.DELETE_CART_DATA_REQUEST})
     return axios
-    .delete(`https://jsondeployment-lyna.vercel.app/bag/${id}`)
+    .delete(`https://json-server-employee-crud-seven.vercel.app/bag/${id}`)
     .then((r)=>{
         dispatch({type:types.DELETE_CART_DATA_SUCCESS,payload:id})
     })
@@ -173,7 +173,7 @@ const deleteCartData=(id)=>(dispatch)=>{
 const deleteWishData=(id)=>(dispatch)=>{
     dispatch({type:types.DELETE_WISH_DATA_REQUEST})
     return axios
-    .delete(`https://jsondeployment-lyna.vercel.app/wish/${id}`)
+    .delete(`https://json-server-employee-crud-seven.vercel.app/wish/${id}`)
     .then((r)=>{
         
         dispatch({type:types.DELETE_WISH_DATA_SUCCESS,payload:id})
